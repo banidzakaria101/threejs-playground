522e311f6226c6510263f8dd3ecc62a0cd03663b
zakariabanid101 <banidzakaria2@gmail.com>
2025-06-28 01:27:40 +0100

    initiakize a vite project, and dreate simple shapes

---
+++ vite-project/src/main.js
@@ -0,0 +1,37 @@
+import * as THREE from 'three';
+
+const canvas = document.getElementById('canvas');
+
+// 1. Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#0F0F0F');
+
+// 2. Camera*
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// 3.Object
+const geometry = new THREE.DodecahedronGeometry();
+const material = new THREE.MeshBasicMaterial({ color: '#468585'})
+const dodecahedron = new THREE.Mesh(geometry, material);
+
+const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
+const boxMaterial = new THREE.MeshBasicMaterial({ color: '#B4B4B3'});
+const box = new THREE.Mesh(boxGeometry, material);
+box.position.y = -1.5;
+
+scene.add(dodecahedron);
+scene.add(box);
+
+// 4.Light
+const light = new THREE.SpotLight(0x006769, 100);
+light.position.set(1, 1, 1);
+scene.add(light);
+
+// 5. Renderer
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.render(scene, camera)
+
+// 6.Add Orbitcontrols
